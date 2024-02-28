1be8d7b1054d9411ae63153060ac9cc1cb217426
gencaypolat <gencaypolat@gmail.com>
2024-02-28 18:20:58 +0300

    Custom Request header added via Postman

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ var app = builder.Build();
 app.Run(async (HttpContext context) =>
 {
     context.Response.Headers["Content-type"] = "text/html";
-    if (context.Request.Headers.ContainsKey("User-Agent"))
+    if (context.Request.Headers.ContainsKey("AuthorizationKey"))
     {
-        string userAgent = context.Request.Headers["User-Agent"];
-        await context.Response.WriteAsync($"<p>{userAgent}<p/>");
+        string auth = context.Request.Headers["AuthorizationKey"];
+        await context.Response.WriteAsync($"<p>{auth}<p/>");
     }
 });
 
