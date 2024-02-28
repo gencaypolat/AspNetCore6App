40dd53adde37c8b193424ae0d7aa23f3a6e9dca5
gencaypolat <gencaypolat@gmail.com>
2024-02-28 04:12:33 +0300

    Http response headers added

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    context.Response.Headers["MyKey"] = "My value";
+    context.Response.Headers["Server"] = "My server";
+    context.Response.Headers["Content-Type"] = "text/html";
+    
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync("<h2>World</h2>");
+});
 
 app.Run();
 
