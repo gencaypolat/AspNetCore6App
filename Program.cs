675071a69cae63a4fecf65e24c8fad32530897bd
gencaypolat <gencaypolat@gmail.com>
2024-02-28 16:16:10 +0300

    Query String added

--- Program.cs
+++ Program.cs
@@ -3,12 +3,15 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    context.Response.Headers["MyKey"] = "My value";
-    context.Response.Headers["Server"] = "My server";
-    context.Response.Headers["Content-Type"] = "text/html";
-    
-    await context.Response.WriteAsync("<h1>Hello</h1>");
-    await context.Response.WriteAsync("<h2>World</h2>");
+    context.Response.Headers["Content-type"] = "text/html";
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
 });
 
 app.Run();
